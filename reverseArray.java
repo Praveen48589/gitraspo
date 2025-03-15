9096f81d6420d5ade3442622a73f0b33e3a4369f
Praveen48589 <tomarpraveen640@gmail.com>
2025-03-15 15:10:10 +0530

    Add files via upload

---
+++ reverseArray.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class reverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the total values : ");
+        int totalValues = sc.nextInt();
+        int[] myArr = new int[totalValues];
+        for(int i = 0;i<totalValues;i++){
+            System.out.print("enter the values"+(i+1)+": ");
+            myArr[i] = sc.nextInt();
+            
+        }
+        reverse(myArr);
+        System.out.print("your array is reverse : ");
+        displayArray(myArr);
+    }
+    
+    public static void reverse(int[] num){
+        int i = 0;
+        while (i<num.length/2) {
+            int swap = num[i];
+            num[i] = num[(num.length-1)-i];
+            num[(num.length-1)-i] = swap;
+            i++;
+
+
+            
+        }
+    }
+
+    
+
+
+    public static int displayArray(int[] num){
+        int i = 0;
+        while (i<num.length) {
+            System.out.print(num[i]+" ");
+            i++;
+        }System.out.println();
+                return i;
+
+    }
+}    
